7e0cef9b5b4a99369b4d415f7cc25ba59dc13561
Elvir Uzunovic <uzunovicelvir@gmail.com>
2019-04-14 17:46:51 +0200

    add input for scores per week

--- javascript/script.js
+++ javascript/script.js
@@ -3,14 +3,14 @@ let html = ``;
 
 function laLiga() {
     let xhttp = new XMLHttpRequest ();
-    let url = `http://api.football-data.org/v2/competitions/PD/matches/?matchday=31`;
+    let week = document.getElementById('week').value;
+    let url = `http://api.football-data.org/v2/competitions/PD/matches/?matchday=` + week;   
     xhttp.onreadystatechange = function() {
         if (xhttp.readyState == 4 && xhttp.status == 200) {
             let result = JSON.parse (xhttp.responseText);
+            console.log(result);
             html = ""
             result.matches.forEach(laLigaResult);
-    console.log(html);
-
             document.getElementById('result').innerHTML = html;
         }
     }
@@ -22,25 +22,35 @@ function laLiga() {
 function laLigaResult(match){
     html +=`
         <tr>
-            <td class="text padd">${match.homeTeam.name}</td>
-            <td class="text">-</td>  
-            <td class="text padd">${match.awayTeam.name}</td>
-            <td class="text">${match.score.fullTime.homeTeam} : ${ match.score.fullTime.awayTeam}</td>
+           <td class="text padd">${match.homeTeam.name}</td>
+           <td class="text">-</td>  
+           <td class="text padd">${match.awayTeam.name}</td>
+           <td class="text">${match.score.fullTime.homeTeam} : ${match.score.fullTime.awayTeam}</td>
         </tr>
      `
 }
 
-function laLigaTable() {
-    let xhttp = new XMLHttpRequest();
-    let url = "http://api.football-data.org/2/competitions/PD/leagueTable";
+function laLigaTable(position) {
+    xhttp = new XMLHttpRequest();
+    let url = `http://api.football-data.org/v2/competitions/PD/standings/?matchday=31`;
     xhttp.onreadystatechange =function() {
         if(xhttp.readyState == 4 && xhttp.status == 200){
             let table = JSON.parse (xhttp.responseText);
+            console.log(table);
+            table.standings.table.forEach(laLigaPos);
+            document.getElementById('standing').innerHTML = html;
             
-        }
-        
+       }
     }
-    xhttp.open ("GET", url, true);
-    xhttp.setRequestHeader ('X-Auth-Token', token);
-    xhttp.send();
+   xhttp.open ("GET", url, true);
+   xhttp.setRequestHeader ('X-Auth-Token', token);
+   xhttp.send();
+}
+
+function laLigaPos(position){
+    html +=`
+        <tr>
+            <td class="text padd">${position.team.name}</td>
+        </tr>
+        `
 }
